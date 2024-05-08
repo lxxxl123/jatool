82012d72d71d037fc6fcf81481dd3d8cf9abd9ec
chenwh3 <chenwh3@haday.cn>
2024-05-08 17:45:41 +0800

    新增二进制工具BinaryUtils

--- jatool-common/src/main/java/com/chen/jatool/common/utils/BinaryUtils.java
+++ jatool-common/src/main/java/com/chen/jatool/common/utils/BinaryUtils.java
@@ -1,26 +1,43 @@
 package com.chen.jatool.common.utils;
 
+import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.StrUtil;
 import com.chen.jatool.common.exception.ServiceException;
 
 /**
  * 二进制工具
+ *
  * @author chenwh3
  */
 public class BinaryUtils {
 
+    public static final String STR_1 = "1";
+    public static final String STR_0 = "0";
+
     public static String to0xb(Object obj, Integer length) {
-        String str ;
+        String str;
         if (obj instanceof Integer) {
             str = Integer.toBinaryString((Integer) obj);
             length = length == null ? 32 : length;
         } else if (obj instanceof Long) {
             str = Long.toBinaryString((Long) obj);
             length = length == null ? 64 : length;
+        } else if (obj instanceof Short) {
+            str = Integer.toBinaryString((Short) obj);
+            length = length == null ? 16 : length;
+        } else if (obj instanceof Byte) {
+            str = Integer.toBinaryString((Byte) obj);
+            length = length == null ? 8 : length;
         } else {
             throw new ServiceException("不支持的类型");
         }
-        return StrUtil.padPre(str, length, "0");
+        if (str.length() >= length) {
+            return str.substring(str.length() - length);
+        } else if (str.startsWith(STR_1)) {
+            return StrUtil.padPre(str, length, STR_1);
+        } else {
+            return StrUtil.padPre(str, length, STR_0);
+        }
     }
 
     public static String to0xb(Object obj) {
@@ -35,7 +52,7 @@ public class BinaryUtils {
     /**
      * 从右到左添加空格
      */
-    private static String appendSpace(String in){
+    private static String appendSpace(String in) {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < in.length(); i++) {
             result.append(in.charAt(i));
