d08f1765ab4c9cf20094cb4f07da5df2e270ff6c
chenwh3 <chenwh3@haday.cn>
2024-06-13 18:06:27 +0800

    新增工具-公式计算-3

--- jatool-common/src/main/java/com/chen/jatool/common/utils/support/Test.java
+++ jatool-common/src/main/java/com/chen/jatool/common/utils/support/Test.java
@@ -1,6 +1,6 @@
 package com.chen.jatool.common.utils.support;
 
-import com.chen.jatool.common.utils.FormularsUtil;
+import com.chen.jatool.common.utils.FormulaUtil;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
@@ -17,14 +17,14 @@ public class Test {
         map.put("a", new BigDecimal(100));
         long current = System.currentTimeMillis();
         BigDecimal res = null;
-        for (int i = 0; i < 10; i++) {
-            res = FormularsUtil.eval("a%+2*(3-4)/5", map);
+        for (int i = 0; i < 1000000; i++) {
+            res = FormulaUtil.eval("a+2*(3-4)/5", map);
         }
         System.out.println(res);
         System.out.println("cost " + (System.currentTimeMillis() - current));
         current = System.currentTimeMillis();
         Formulas formulas = Formulas.of("a+2*(3-4)/5");
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1000000; i++) {
             res = formulas.cals((Map) map).getDecimal();
         }
         System.out.println(res);
