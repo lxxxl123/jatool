9fbdd95e73a616006c06f3b036fbdc75f12a2159
chenwh3 <chenwh3@haday.cn>
2024-07-05 17:59:17 +0800

    工具优化

--- jatool-common/src/main/java/com/chen/jatool/common/utils/CollUtils.java
+++ jatool-common/src/main/java/com/chen/jatool/common/utils/CollUtils.java
@@ -3,6 +3,8 @@ package com.chen.jatool.common.utils;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.convert.Convert;
 import cn.hutool.core.util.StrUtil;
+import com.chen.jatool.common.exception.ServiceException;
+import com.chen.jatool.common.utils.support.string.MessageBuilder;
 import org.apache.commons.lang3.StringUtils;
 
 import java.lang.reflect.Array;
@@ -25,6 +27,15 @@ public class CollUtils {
         return t -> seen.add(keyExtractor.apply(t));
     }
 
+    public static <T> Predicate<T> repeatByKey(Function<? super T, ?> keyExtractor) {
+        Set<Object> seen = ConcurrentHashMap.newKeySet();
+        Set<Object> existed = ConcurrentHashMap.newKeySet();
+        return t -> {
+            Object val = keyExtractor.apply(t);
+            return !seen.add(val) && existed.add(val);
+        };
+    }
+
     public static boolean isAnyEmpty(Collection<?>... colls) {
         return Arrays.stream(colls).anyMatch(CollUtil::isEmpty);
     }
@@ -33,6 +44,16 @@ public class CollUtils {
     public static <T> Predicate<T> distinctByKeys(Function<? super T, ?>... keyExtractors) {
         Set<Object> seen = ConcurrentHashMap.newKeySet();
         return t -> seen.add(Arrays.stream(keyExtractors).map(e -> e.apply(t)).collect(Collectors.toList()));
+    }
+
+    @SafeVarargs
+    public static <T> Predicate<T> repeatByKeys(Function<? super T, ?>... keyExtractors) {
+        Set<Object> seen = ConcurrentHashMap.newKeySet();
+        Set<Object> existed = ConcurrentHashMap.newKeySet();
+        return t -> {
+            List<?> val = Arrays.stream(keyExtractors).map(e -> e.apply(t)).collect(Collectors.toList());
+            return !seen.add(val) && existed.add(val);
+        };
     }
 
 
@@ -72,7 +93,12 @@ public class CollUtils {
         }
     }
 
+    @Deprecated
     public static Collection<?> toColl(Object obj) {
+        return toStream(obj).collect(Collectors.toList());
+    }
+
+    public static List<?> toList(Object obj) {
         return toStream(obj).collect(Collectors.toList());
     }
 
@@ -95,10 +121,14 @@ public class CollUtils {
         return Arrays.stream(obj).flatMap(CollUtils::toStream).map(StrUtil::toStringOrNull).filter(StringUtils::isNotBlank).collect(Collectors.toList());
     }
 
+    @Deprecated
     public static Collection<Integer> toIntColl(Object obj) {
         return toStream(obj).map(Convert::toInt).collect(Collectors.toList());
     }
 
+    public static List<Integer> toIntList(Object obj) {
+        return toStream(obj).map(Convert::toInt).filter(Objects::nonNull).collect(Collectors.toList());
+    }
 
     public static <T> List<List<T>> partitionList(List<T> list, int pageSize) {
         if (list == null || list.isEmpty()) {
@@ -148,6 +178,57 @@ public class CollUtils {
         return list.stream()
                 .map(e -> Convert.convert(clazz, ObjectUtil.get(e, key), null))
                 .min((a, b) -> a.compareTo(b)).get();
+    }
+
+    public static boolean containsAny(Collection<?> coll1, Collection<?> coll2){
+        if (CollUtil.isEmpty(coll1) || CollUtil.isEmpty(coll2)) {
+            return false;
+        }
+        Collection<?> doFor;
+        Collection<?> doContains;
+        if (coll1 instanceof Set && !(coll2 instanceof Set)) {
+            doContains = coll1;
+            doFor = coll2;
+        } else if (coll2 instanceof Set && !(coll1 instanceof Set)) {
+            doContains = coll2;
+            doFor = coll1;
+        } else if (coll1.size() < coll2.size()) {
+            doContains = coll2;
+            doFor = coll1;
+        } else {
+            doContains = coll1;
+            doFor = coll2;
+        }
+        for (Object o : doFor) {
+            if (doContains.contains(o)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static <T> void checkDuplicate(List<T> list, List<String> fieldNames, List<Function<T, ?>> keyExtractors) {
+        MessageBuilder mb = new MessageBuilder("数据重复：</br>", "</br>", "");
+        list.stream().filter(CollUtils.repeatByKeys(keyExtractors.toArray(new Function[]{})))
+                .forEach(e -> {
+                    mb.append("[ ");
+                    for (int i = 0; i < keyExtractors.size(); i++) {
+                        //必须强转，删除后会编译失败
+                        Object apply = keyExtractors.get(i).apply((T) e);
+                        String field = CollUtil.get(fieldNames, i);
+                        if (field != null) {
+                            mb.append("{}={}", CollUtil.get(fieldNames, i), apply);
+                        } else {
+                            mb.append("{}", CollUtil.get(fieldNames, i), apply);
+                        }
+                        if (i != fieldNames.size() - 1) mb.append("，");
+                    }
+                    mb.append(" ]");
+                });
+        if (mb.containMsg()) {
+            throw new ServiceException(mb.toString());
+        }
     }
 
 
--- jatool-common/src/main/java/com/chen/jatool/common/utils/ObjectUtil.java
+++ jatool-common/src/main/java/com/chen/jatool/common/utils/ObjectUtil.java
@@ -3,19 +3,26 @@ package com.chen.jatool.common.utils;
 import cn.hutool.core.convert.Convert;
 import cn.hutool.core.util.ReflectUtil;
 import cn.hutool.core.util.StrUtil;
+import com.chen.jatool.common.exception.ServiceException;
+import com.chen.jatool.common.utils.support.Numbers;
 import org.springframework.lang.Nullable;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 /**
  * 对象工具类
  *
  * @author L.cm
  */
-public class ObjectUtil extends cn.hutool.core.util.ObjectUtil {
+public class ObjectUtil extends org.springframework.util.ObjectUtils {
 
     /**
      * 判断元素不为空
+     *
      * @param obj object
      * @return boolean
      */
@@ -27,8 +34,21 @@ public class ObjectUtil extends cn.hutool.core.util.ObjectUtil {
         return !isBlank(obj);
     }
 
-    public static boolean isBlank(Object obj){
+    public static boolean isBlank(Object obj) {
         return obj == null || obj instanceof String && StrUtil.isBlank((String) obj);
+    }
+
+    public static boolean equal(Object obj1, Object obj2) {
+        return Objects.equals(obj1, obj2);
+    }
+
+    public static boolean equalsWithAny(Object obj, Object... any) {
+        for (Object o : any) {
+            if (ObjectUtil.equal(obj, o)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static Object tryTrim(Object obj) {
@@ -60,6 +80,31 @@ public class ObjectUtil extends cn.hutool.core.util.ObjectUtil {
         } else {
             ReflectUtil.setFieldValue(obj, field, val);
         }
+    }
+
+    public static Object merge(Object obj1, Object obj2) {
+        if (obj1 == null) {
+            return obj2;
+        }
+        if (obj2 == null) {
+            return obj1;
+        }
+        if (obj1 instanceof Collection || obj2 instanceof Collection
+                || obj1 instanceof Stream || obj2 instanceof Stream
+                || obj1 instanceof Iterable || obj2 instanceof Iterable) {
+            return Convert.convert(obj1.getClass(), CollUtils.toList(obj1).addAll((Collection) CollUtils.toList(obj2)));
+        }
+
+        if (obj1 instanceof Map && obj2 instanceof Map) {
+            HashMap<Object, Object> map = new HashMap<>();
+            map.putAll((Map) obj1);
+            map.putAll((Map) obj2);
+            return map;
+        }
+        if (obj1 instanceof Number && obj2 instanceof Number) {
+            return Convert.convert(obj1.getClass(), Numbers.of(obj1).add(obj2).getDecimal());
+        }
+        throw new ServiceException("not support merge type between {} and {}", obj1.getClass(), obj2.getClass());
     }
 
 }
