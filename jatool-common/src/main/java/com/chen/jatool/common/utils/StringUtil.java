0181d679c4083a8ac077c5c0ea3c6bd9a3fe0f7f
chenwh3 <chenwh3@haday.cn>
2025-01-21 16:13:11 +0800

    工具类更新

--- jatool-common/src/main/java/com/chen/jatool/common/utils/StringUtil.java
+++ jatool-common/src/main/java/com/chen/jatool/common/utils/StringUtil.java
@@ -3,7 +3,18 @@ package com.chen.jatool.common.utils;
 public class StringUtil {
 
     public static boolean isChinese(char ch){
-        return ch >= 0x4E00 && ch <= 0x9FA5;
+        //获取此字符的UniCodeBlock
+        Character.UnicodeBlock ub = Character.UnicodeBlock.of(ch);
+        //  GENERAL_PUNCTUATION 判断中文的“号
+        //  CJK_SYMBOLS_AND_PUNCTUATION 判断中文的。号
+        //  HALFWIDTH_AND_FULLWIDTH_FORMS 判断中文的，号
+        return (ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A
+                || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_B
+                || ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION
+                || ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS
+                || ub == Character.UnicodeBlock.GENERAL_PUNCTUATION);
     }
 
     public static String toStringOrElse(Object obj, String defaultValue) {
